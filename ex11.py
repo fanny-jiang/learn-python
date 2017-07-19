9c0b585c27caaab1e7716c5571678344e71efba6
Fanny Jiang <jiangf@bu.edu>
2017-07-19 17:37:45 -0400

    completed ex11

--- ex11.py
+++ ex11.py
@@ -1,10 +1,15 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weigh?",
+# weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+# print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
 
 # what does raw_input() do? It kind of looks like javascript prompt
+
+print "How old are you?"
+age = raw_input('--> ')
+
+print "I am %r years old." % (age)
